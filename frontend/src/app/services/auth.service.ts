398cc79ae2dadbd71d82548009dac24352d0651f
Leng Yang <lengyang@Lengs-MacBook-Pro.local>
2023-01-21 17:04:52 -0600

    add auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { LOGIN } from '../mutations/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _apollo: Apollo) { }
+
+  login(loginObj: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this._apollo.mutate({
+        mutation: LOGIN,
+        variables: loginObj
+      })
+      .subscribe((res: any) => {
+        
+      })
+    })
+  }
+
 }
